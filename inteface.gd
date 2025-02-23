1358be06c1b7eeb08ae8e2163eea0d28d45f3f6d
Bruno <95176651+koddoki@users.noreply.github.com>
2025-02-23 17:02:41 -0300

    Slider slida

--- inteface.gd
+++ inteface.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_color_button_pega_a_cor_ai(cor: Color) -> void:
 	Painter.color = cor
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	Painter.thickness = value
