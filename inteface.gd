66e0f3f0f488df520e1b6b5c59f8b75fbecc602c
Bruno <95176651+koddoki@users.noreply.github.com>
2025-02-23 17:08:25 -0300

    interface is over (probably)(

--- inteface.gd
+++ inteface.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal submit
+
 
 func _on_color_button_pega_a_cor_ai(cor: Color) -> void:
 	Painter.color = cor
@@ -7,3 +9,7 @@ func _on_color_button_pega_a_cor_ai(cor: Color) -> void:
 
 func _on_h_slider_value_changed(value: float) -> void:
 	Painter.thickness = value
+
+
+func _on_submit_button_pressed() -> void:
+	submit.emit()
